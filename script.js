d81f3afce62cea8eeced009dfc6ac17ec1271af8
Shaili-Patel <526shailipatel@frhsd.com>
2025-04-23 09:31:46 -0400

    characters

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ let detectiveName = `
 console.log(detectiveName);
 
 document.getElementById('detective').innerHTML = detectiveName;
+
+document.querySelector('.start').addEventListener('click', () => {
+  window.location.href = "game.html";
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.querySelector('.start');
+  const startScreen = document.getElementById('startScreen');
+  const gameScreen = document.getElementById('gameScreen');
+
+  startButton.addEventListener('click', () => {
+    startScreen.classList.add('d-none');    // hide start screen
+    gameScreen.classList.remove('d-none');  // show game screen
+  });
+});
+
+
+function openCharacterPage(character) {
+  // Open a new page based on the character clicked
+  window.location.href = character + '.html'; // Example: 'evelyn.html'
+}
